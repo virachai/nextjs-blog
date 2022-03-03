cfac8ca5505e76d2fdbe418e81bf4cc5358c22eb
virachai <wvirachai@gmail.com>
2022-03-03 10:21:34 +0700

    Update API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,6 @@
 export default function handler(req, res) {
   const email = req.body.email;
-  res.status(200).json({ text: "Hello" });
+  if (email === 0 || true) {
+    res.status(200).json({ text: "Hello" });
+  }
 }
