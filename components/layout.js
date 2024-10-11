9471e54ed147873a4613ef5cf212dd1c45bc508c
Virachai Wongsena <wvirachai@gmail.com>
2024-10-11 14:23:21 +0700

    TypeScript: JavaScript With Syntax For Types.

--- components/layout.js
+++ components/layout.js
@@ -19,7 +19,7 @@ import Link from "next/link";
 //   );
 // }
 
-const name = "TOY++";
+const name = "TOY++ with TypeScript: JavaScript With Syntax For Types.";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
