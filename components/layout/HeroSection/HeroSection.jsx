cf27e24c938b57420f7964e306132f657e2cb56d
Virachai Wongsena <wvirachai@gmail.com>
2024-10-17 18:52:53 +0700

    sm hero section responsive is work

--- components/layout/HeroSection/HeroSection.jsx
+++ components/layout/HeroSection/HeroSection.jsx
@@ -8,22 +8,22 @@ export default function HeroSection() {
   return (
     <section className={`w-full`}>
       <div
-        className={`relative overflow-y-hidden md:max-h-[620px] lg:max-h-[737px]`}
+        className={`relative overflow-y-hidden max-h-[500px] md:max-h-[620px] lg:max-h-[737px]`}
       >
         <div
           className={`${styles.home_slider_item}`}
           data-bg-img="https://template.hasthemes.com/kidol/kidol/assets/img/slider/3.jpg"
         ></div>
         <div className={`${styles.slider_content_area}`}>
-          <div className="container flex justify-end">
-            <div className="row sm:w-2/3 md:w-7/12 lg:w-1/2">
-              <div className=" flex justify-end md:justify-center lg:justify-start p-2">
-                <div className="content content2">
+          <div className="container flex justify-center md:justify-end">
+            <div className="row w-[370px] md:w-7/12 lg:w-1/2">
+              <div className=" flex justify-center lg:justify-start">
+                <div className="content max-w-[400px]">
                   <div className="inner-content">
-                    <h4 className="subtitle text-base text-center mb-[22px]">
+                    <h4 className="subtitle text-xl text-center mb-[22px]">
                       New Arrivals
                     </h4>
-                    <h2 className="text-5xl text-center mb-[45px]">
+                    <h2 className="text-4xl sm:text-5xl text-center mb-[45px]">
                       Best&nbsp;Kids&nbsp;Store &amp;&nbsp;Online&nbsp;Shop
                     </h2>
                     <Link href="/toy" className={`${styles.bt}`}>
@@ -35,11 +35,6 @@ export default function HeroSection() {
             </div>
           </div>
         </div>
-        {/* <div
-          className="shape-top bg-img"
-          data-bg-img="assets/img/photos/1.png"
-          style='background-image: url("assets/img/photos/1.png");'
-        ></div> */}
       </div>
     </section>
   );
