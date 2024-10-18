9daf39a34e8477c111f72623437812cd284854a1
Virachai Wongsena <wvirachai@gmail.com>
2024-10-18 17:07:30 +0700

    gap catalog in work

--- components/layout/CategorieArea/CategorieArea.jsx
+++ components/layout/CategorieArea/CategorieArea.jsx
@@ -4,7 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./CategorieArea.module.css";
 
 export default function CategorieArea() {
-  const activities = [
+  const catalog = [];
+  const list = [
     "Pretend Play",
     "Learning & Education",
     "Music",
@@ -12,13 +13,34 @@ export default function CategorieArea() {
     "Active Play",
   ];
 
+  list.forEach((element, i) => {
+    catalog.push(
+      <div
+        key={i}
+        className="overflow-hidden max-w-[255px] min-w-[240px] relative"
+      >
+        <Image
+          className="w-full rounded-[36px] overflow-hiddenW"
+          src="/images/300x300.avif"
+          alt="Pretend Play"
+          width={300}
+          height={300}
+        />
+        <div className="py-2">
+          <div className="text-base text-gray-700">{element}</div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <section className={`w-full`}>
       <div className={`container w-auto text-center`}>
         <h2 className={`title text-4xl sm:mb-4 md:mb-6`}>Categories</h2>
         <div className={`row`}>
-          <div className={`col-12`}>
-            <div className="overflow-hidden max-w-[255px] relative">
+          <div className={`col-12 flex justify-between gap-2`}>
+            {catalog}
+            {/* <div className="overflow-hidden max-w-[255px] relative">
               <Image
                 className="w-full rounded-[36px] overflow-hiddenW"
                 src="/images/300x300.avif"
@@ -29,7 +51,7 @@ export default function CategorieArea() {
               <div className="py-2">
                 <div className="text-base text-gray-900">Pretend Play</div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
